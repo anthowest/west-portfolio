94252448e0f9dc614260135e7742fa80ff24337f
anthowest <anthowest14@gmail.com>
2022-10-26 14:56:00 -0500

    Social icons

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,6 +4,7 @@ import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
     <div className='nav-bar'>
@@ -12,16 +13,50 @@ const Sidebar = () => (
             <img className='sub-logo' src={LogoSubtitle} alt='subtitle' />
         </Link>
         <nav>
-            <NavLink exact="true" activeclassname="active" to="/">
+            <NavLink 
+            exact="true" 
+            activeclassname="active" 
+            to="/"
+            >
                 <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
             </NavLink>
-            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+            <NavLink 
+            exact="true" 
+            activeclassname="active" 
+            className="about-link" 
+            to="/about"
+            >
                 <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
             </NavLink>
-            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+            <NavLink 
+            exact="true" 
+            activeclassname="active" 
+            className="contact-link" 
+            to="/contact"
+            >
                 <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
             </NavLink>
         </nav>
+        <ul>
+            <li>
+                <a 
+                target="_blank" 
+                rel='noreferrer' 
+                href='https://www.linkedin.com/in/anthonypwest/'
+                >
+                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+                </a>
+            </li>
+            <li>
+                <a 
+                target="_blank" 
+                rel='noreferrer' 
+                href='https://github.com/anthowest'
+                >
+                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                </a>
+            </li>
+            </ul>
     </div>
 )
 
